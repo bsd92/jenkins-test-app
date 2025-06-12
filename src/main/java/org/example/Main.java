a7bfac7deeba5ff6cb391e01891af2a0696bb831
bsd92 <boubacarhb92@gmail.com>
2025-06-12 14:06:19 +0200

    modif display

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World of Boubacar 15h34");
+        System.out.println("Hello, World of Boubacar du 12 juin 14h05");
     }
 }
