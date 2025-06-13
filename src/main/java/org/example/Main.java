307fba6d6a6312c95d8734a32fb829b29b18ef36
bsd92 <boubacarhb92@gmail.com>
2025-06-13 15:00:10 +0200

    test3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World of Boubacar du 12 juin 14h05");
+        System.out.println("Hello, World of Boubacar du 13 juin");
     }
 }
